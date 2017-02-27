2fd397ac28d03bfb39d2332d44d6cf87088d35ee
renhongguang <renhongguang@longtugame.com>
2017-02-27 17:21:45 +0800

    no message

--- CrossApp/control/CACheckbox.cpp
+++ CrossApp/control/CACheckbox.cpp
@@ -57,6 +57,8 @@ CACheckbox::CACheckbox()
     const CAThemeManager::stringMap& map = CAApplication::getApplication()->getThemeManager()->getThemeMap("CACheckbox");
     this->setImageStateNormal(CAImage::create(map.at("checkbox_image_normal")));
     this->setImageStateSelected(CAImage::create(map.at("checkbox_image_selected")));
+    this->setTitleColorStateNormal(ccc4Int(CrossApp::hex2Int(map.at("titleColor_normal"))));
+    this->setTitleColorStateSelected(ccc4Int(CrossApp::hex2Int(map.at("titleColor_selected"))));
 }
 
 CACheckbox::~CACheckbox(void)
