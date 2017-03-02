58cd8b507ae10481ee6fdb62c4a7d8517dfc6bc7
梁元 <744006855@qq.com>
2017-03-02 15:31:21 +0800

    no message

--- samples/Test/Classes/Control/StepperTest.cpp
+++ samples/Test/Classes/Control/StepperTest.cpp
@@ -43,51 +43,51 @@ void StepperTest::viewDidLoad()
     
     if (StepperNum == 0)
     {
-        step_value = CALabel::createWithLayout(DLayout(DHorizontalLayout_W_C(300, 0.5), DVerticalLayout_H_C(100, 0.25)));
-        step_value->setText("step_value:0");
-        step_value->setColor(CAColor_black);
-        step_value->setFontSize(28);
-        step_value->setTextAlignment(CATextAlignment::Center);
-        step_value->setVerticalTextAlignmet(CAVerticalTextAlignment::Center);
-        this->getView()->addSubview(step_value);
+        CALabel* stepValue = CALabel::createWithLayout(DLayout(DHorizontalLayout_W_C(300, 0.5), DVerticalLayout_H_C(100, 0.25)));
+        stepValue->setText("step_value:0");
+        stepValue->setColor(CAColor_black);
+        stepValue->setFontSize(28);
+        stepValue->setTextAlignment(CATextAlignment::Center);
+        stepValue->setVerticalTextAlignmet(CAVerticalTextAlignment::Center);
+        this->getView()->addSubview(stepValue);
         
-        step = CAStepper::createWithLayout(DLayout(DHorizontalLayout_W_C(360, 0.5), DVerticalLayout_H_C(60, 0.5)), CAStepper::Orientation::Horizontal);
-        //step->setWraps(true);//是否循环,默认循环
-        step->setMinValue(0);
-        step->setMaxValue(50);
-        step->setStepValue(1);
-        //step->setAutoRepeat(false);
-        this->getView()->addSubview(step);
-        step->setTarget([=](float value)
+        CAStepper* stepper = CAStepper::createWithLayout(DLayout(DHorizontalLayout_W_C(360, 0.5), DVerticalLayout_H_C(60, 0.5)), CAStepper::Orientation::Horizontal);
+        //stepper->setWraps(true);//是否循环,默认循环
+        stepper->setMinValue(0);
+        stepper->setMaxValue(50);
+        stepper->setStepValue(1);
+        //stepper->setAutoRepeat(false);
+        this->getView()->addSubview(stepper);
+        stepper->setTarget([=](float value)
         {
             char tem[30];
             sprintf(tem, "step_value:%.0f",value);
-            step_value->setText(tem);
+            stepValue->setText(tem);
             CCLog("step-tag === %f",value);
         });
     }
     else
     {
-        step_value = CALabel::createWithLayout(DLayout(DHorizontalLayout_W_C(300, 0.5), DVerticalLayout_H_C(100, 0.25)));
-        step_value->setText("step_value:0");
-        step_value->setColor(CAColor_black);
-        step_value->setFontSize(28);
-        step_value->setTextAlignment(CATextAlignment::Center);
-        step_value->setVerticalTextAlignmet(CAVerticalTextAlignment::Center);
-        this->getView()->addSubview(step_value);
+        CALabel* stepValue1 = CALabel::createWithLayout(DLayout(DHorizontalLayout_W_C(300, 0.5), DVerticalLayout_H_C(100, 0.25)));
+        stepValue1->setText("step_value:0");
+        stepValue1->setColor(CAColor_black);
+        stepValue1->setFontSize(28);
+        stepValue1->setTextAlignment(CATextAlignment::Center);
+        stepValue1->setVerticalTextAlignmet(CAVerticalTextAlignment::Center);
+        this->getView()->addSubview(stepValue1);
         
-        step = CAStepper::createWithLayout(DLayout(DHorizontalLayout_W_C(60, 0.5), DVerticalLayout_H_C(360, 0.5)), CAStepper::Orientation::Vertical);
-        //step->setWraps(true);//是否循环,默认循环
-        step->setMinValue(0);
-        step->setMaxValue(50);
-        step->setStepValue(1);
-        //step->setAutoRepeat(false);
-        this->getView()->addSubview(step);
-        step->setTarget([=](float value)
+        CAStepper* stepper1 = CAStepper::createWithLayout(DLayout(DHorizontalLayout_W_C(60, 0.5), DVerticalLayout_H_C(360, 0.5)), CAStepper::Orientation::Vertical);
+        //stepper1->setWraps(true);//是否循环,默认循环
+        stepper1->setMinValue(0);
+        stepper1->setMaxValue(50);
+        stepper1->setStepValue(1);
+        //stepper1->setAutoRepeat(false);
+        this->getView()->addSubview(stepper1);
+        stepper1->setTarget([=](float value)
         {
             char tem[30];
             sprintf(tem, "step_value:%.0f",value);
-            step_value->setText(tem);
+            stepValue1->setText(tem);
             CCLog("step-tag === %f",value);
         });
     }
