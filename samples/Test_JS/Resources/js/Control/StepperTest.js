a39538e6654633803d6bd22ded07e5d089f21e7f
梁元 <744006855@qq.com>
2017-03-03 15:49:22 +0800

    no message

--- samples/Test_JS/Resources/js/Control/StepperTest.js
+++ samples/Test_JS/Resources/js/Control/StepperTest.js
@@ -2,9 +2,14 @@
  * Created by zhanglei on 16/8/3.
  */
 var StepperTest = ca.CAViewController.extend({
+    StepperNum : 1,
     step_value : null,
-    ctor: function () {
+    maxNum:1,
+    ctor: function (num) {
         this._super();
+        if(num != null){
+            this.StepperNum = num;
+        }
         this.initStepper();
     },
     initStepper:function(){
@@ -17,20 +22,37 @@ var StepperTest = ca.CAViewController.extend({
         this.step_value.setVerticalTextAlignmet(ca.CAVerticalTextAlignment.Center);
         this.getView().addSubview(this.step_value);
 
-
-        var step = ca.CAStepper.createWithLayout(DLayout(DHorizontalLayout_W_C(360, 0.5), DVerticalLayout_H_C(60, 0.5)), ca.CAStepper.Orientation.Horizontal);
-        //step.setWraps(true);//是否循环,默认循环
-        step.setMinValue(0);
-        step.setMaxValue(50);
-        step.setStepValue(1);
-        //step.setAutoRepeat(false);
-        this.getView().addSubview(step);
-        var valueLabel = this.step_value;
-        step.setTarget(function (value) {
-            log("stepperCallBack-->");
-            var str = "step_value:" + value;
-            valueLabel.setText(str);
-        });
-
+        if (this.StepperNum == 0)
+        {
+            var step = ca.CAStepper.createWithLayout(DLayout(DHorizontalLayout_W_C(360, 0.5), DVerticalLayout_H_C(60, 0.5)), ca.CAStepper.Orientation.Horizontal);
+            //step.setWraps(true);//是否循环,默认循环
+            step.setMinValue(0);
+            step.setMaxValue(50);
+            step.setStepValue(1);
+            //step.setAutoRepeat(false);
+            this.getView().addSubview(step);
+            var valueLabel = this.step_value;
+            step.setTarget(function (value) {
+                log("stepperCallBack-->");
+                var str = "step_value:" + value;
+                valueLabel.setText(str);
+            });
+        }
+        else
+        {
+            var step = ca.CAStepper.createWithLayout(DLayout(DHorizontalLayout_W_C(360, 0.5), DVerticalLayout_H_C(60, 0.5)), ca.CAStepper.Orientation.Vertical);
+            //step.setWraps(true);//是否循环,默认循环
+            step.setMinValue(0);
+            step.setMaxValue(50);
+            step.setStepValue(1);
+            //step.setAutoRepeat(false);
+            this.getView().addSubview(step);
+            var valueLabel = this.step_value;
+            step.setTarget(function (value) {
+                log("stepperCallBack-->");
+                var str = "step_value:" + value;
+                valueLabel.setText(str);
+            });
+        }
     },
 });
