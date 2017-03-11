0791f8ed9f2d27e28d3a051bcbc209282c289cef
秦乐 <jake_qin@163.com>
2017-03-11 17:19:36 +0800

    no message

--- CrossApp/platform/mac/CATextView.mm
+++ CrossApp/platform/mac/CATextView.mm
@@ -31,11 +31,12 @@
     
 }
 
-- (instancetype)initWithFrame:(NSRect)frameRect
+-(id)initWithFrame:(NSRect)frameRect
 {
     if ([super initWithFrame:frameRect])
     {
         [self setBackgroundColor:[NSColor clearColor]];
+        [self setBezeled:NO];
         return self;
     }
     return nil;
@@ -222,7 +223,6 @@ CATextView::CATextView()
     EAGLView * eaglview = [EAGLView sharedEGLView];
     [eaglview addSubview:textView_Mac];
     textView_Mac.textView = this;
-    [textView_Mac setBackgroundColor:[NSColor clearColor]];
     [textView_Mac setText:@""];
     [textView_Mac release];
     
