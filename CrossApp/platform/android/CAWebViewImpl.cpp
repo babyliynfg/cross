7db4182410275f1725c9f1542e90602dcf97eb67
LiuGuoyan <liuguoyan21@126.com>
2017-05-27 18:10:43 +0800

    no message

--- CrossApp/platform/android/CAWebViewImpl.cpp
+++ CrossApp/platform/android/CAWebViewImpl.cpp
@@ -398,7 +398,7 @@ CAImageView* CAWebViewImpl::getWebViewImage()
 		DSize size = _webView->getBounds().size;
 
         CrossApp::CAData* ca_data = CrossApp::CAData::create();
-        ca_data->fastSet((const unsigned char*)&s_cszWebViewImageData[0], s_cszWebViewImageData.size());
+        ca_data->fastSet((unsigned char*)&s_cszWebViewImageData[0], s_cszWebViewImageData.size());
         
 		CAImage* pImage = new CAImage();
 		if (!pImage->initWithRawData(ca_data, CAImage::PixelFormat::RGBA8888, size.width, size.height))
