4d42d9ecd54e9a8798e9c746e4663a4f148518a0
liyuanfeng <278688386@qq.com>
2017-06-27 12:14:48 +0800

    no message

--- CrossApp/platform/CAFontProcesstor.h
+++ CrossApp/platform/CAFontProcesstor.h
@@ -8,17 +8,17 @@
 NS_CC_BEGIN
 
 class CAImage;
-namespace CAFontProcesstor
+struct CC_DLL CAFontProcesstor
 {
-    CAImage* imageForRichText(const std::vector<CARichLabel::Element>& elements, DSize& dimensions, CATextAlignment textAlignment);
+    static CAImage* imageForRichText(const std::vector<CARichLabel::Element>& elements, DSize& dimensions, CATextAlignment textAlignment);
     
-    CAImage* imageForText(const std::string& text, const CAFont& font, DSize& dimensions, CATextAlignment textAlignment);
+    static CAImage* imageForText(const std::string& text, const CAFont& font, DSize& dimensions, CATextAlignment textAlignment);
     
-    float heightForFont(const CAFont& font);
+    static float heightForFont(const CAFont& font);
     
-    float heightForTextAtWidth(const std::string& text, const CAFont& font, float width);
+    static float heightForTextAtWidth(const std::string& text, const CAFont& font, float width);
     
-    float widthForTextAtOneLine(const std::string& text, const CAFont& font);
-}
+    static float widthForTextAtOneLine(const std::string& text, const CAFont& font);
+};
 NS_CC_END
 #endif // __CAFontProcesstor_H__
