b863b8cac3e4dbc6bae2547635fdcebcddafaf49
Li Yuanfeng <278688386@qq.com>
2018-05-11 17:20:01 +0800

    no message

--- CrossApp/ccTypes.h
+++ CrossApp/ccTypes.h
@@ -217,7 +217,7 @@ namespace script
     static int viewDidUnload = 0x12;
     static int viewSizeDidChanged = 0x13;
     static int viewDidAppear = 0x14;
-    static int viewDidDisappear = 0x14;
+    static int viewDidDisappear = 0x15;
 }
 
 
