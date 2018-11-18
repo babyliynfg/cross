e5f1dea605aea20ad05d1cb5b555388e687ab79e
Li Yuanfeng <278688386@qq.com>
2018-11-19 00:28:49 +0800

    no message

--- scripting/js-bindings/manual/android/CCJavascriptJavaBridge.cpp
+++ scripting/js-bindings/manual/android/CCJavascriptJavaBridge.cpp
@@ -1,25 +1,4 @@
-/*
- * Copyright (c) 2013-2014 Chukong Technologies Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
- 
+
 #include <android/log.h>
 #include "CCJavascriptJavaBridge.h"
 
@@ -39,14 +18,8 @@ extern "C" {
 JNIEXPORT jint JNICALL Java_org_crossapp_lib_CrossAppHelper_evalString
   (JNIEnv *env, jclass cls, jstring value)
 {
-    bool strFlag = false;
-    std::string strValue = CrossApp::StringUtils::getStringUTFCharsJNI(env, value, &strFlag);
-    if (!strFlag)
-    {
-        CCLOG("evalString error, invalid string code");
-        return 0;
-    }
-    ScriptingCore::getInstance()->evalString(strValue.c_str());
+    const char* strValue = env->GetStringUTFChars(value, 0);
+    ScriptingCore::getInstance()->evalString(strValue);
     return 1;
 }
 
