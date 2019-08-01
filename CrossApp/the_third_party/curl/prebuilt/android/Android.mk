c4d390c0af0d5d52f00db30ae8a70967dcae2b68
Li Yuanfeng <278688386@qq.com>
2019-08-01 18:14:51 +0800

    no message

--- CrossApp/the_third_party/curl/prebuilt/android/Android.mk
+++ CrossApp/the_third_party/curl/prebuilt/android/Android.mk
@@ -5,8 +5,6 @@ LOCAL_MODULE := CrossApp_curl_static
 LOCAL_MODULE_FILENAME := curl
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcurl.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
-
 LOCAL_STATIC_LIBRARIES += ext_ssl
 LOCAL_STATIC_LIBRARIES += ext_crypto
-
 include $(PREBUILT_STATIC_LIBRARY)
