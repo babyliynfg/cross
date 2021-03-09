f0a2c3db43a1af690a5135452131e2e76285d5ae
栗元峰 <278688386@qq.com>
2021-03-10 00:58:14 +0800

    no message

--- samples/Test_JS/Classes/AppDelegate.cpp
+++ samples/Test_JS/Classes/AppDelegate.cpp
@@ -14,51 +14,46 @@ AppDelegate::AppDelegate()
 
 AppDelegate::~AppDelegate()
 {
-
+    
 }
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
-	// initialize director
-	CAApplication* applicationn = CAApplication::getApplication();
+    CAApplication* applicationn = CAApplication::getApplication();
+    applicationn->setClearColor(CAColor4F(0, 0, 0, 0));
+    CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
+    
+    applicationn->setOpenGLView(pEGLView);
+    
+    ScriptingCore* sc = ScriptingCore::getInstance();
+    sc->addRegisterCallback(register_all_crossapp);
+    sc->addRegisterCallback(register_crossapp_js_core);
+    sc->addRegisterCallback(register_all_crossapp_delegates_manual);
+    sc->addRegisterCallback(register_jsb_websocket);
+    sc->start();
+    sc->enableDebugger();
+    
+    CAScriptEngineManager::createScriptEngineManager()->setScriptEngine(sc);
+    
+    sc->runScript("js/main.js");
 
-	CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
-
-	applicationn->setOpenGLView(pEGLView);
-
-	ScriptingCore* sc = ScriptingCore::getInstance();
-
-	sc->addRegisterCallback(register_all_crossapp);
-	sc->addRegisterCallback(register_crossapp_js_core);
-	sc->addRegisterCallback(register_all_crossapp_delegates_manual);
-	sc->addRegisterCallback(register_jsb_websocket);
-	sc->start();
-	sc->enableDebugger();
-
-	sc->runScript("script/jsb_boot.js");
-	sc->runScript("script/jsb_crossapp.js");
-
-	CAScriptEngineManager::createScriptEngineManager()->setScriptEngine(sc);
-
-	sc->runScript("js/main.js");
-
-	return true;
+    return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
-	CAApplication::getApplication()->stopAnimation();
+    CAApplication::getApplication()->stopAnimation();
 
-	// if you use SimpleAudioEngine, it must be pause
-	// SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
-	CAApplication::getApplication()->startAnimation();
+    CAApplication::getApplication()->startAnimation();
 
-	// if you use SimpleAudioEngine, it must resume here
-	// SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
 }
